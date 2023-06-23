544957661d275ef709c464afc2cbd9499598069f
victor <victor3790@gmail.com>
2023-06-23 10:26:47 -0600

    jsTree taxonomy front end functionality added.

--- assets/taxonomy.js
+++ assets/taxonomy.js
@@ -1,47 +1,75 @@
-//Initialize the main section controller.
-acf.addAction('new_field/type=produCustomTaxonomyField', function( field ){
-    let props = {
-        allowNull: false,
-        placeholder: 'Select',
-        multiple: true,
-        ajax: true,
-        ajaxAction: 'acf/fields/taxonomy/query',
-        ajaxData: function (data) {
-          data.field_key = field.get('key');
-          return data;
+jQuery(function ($) {
+  //What to do when a main category is selected.
+  PRODU_DATA.select.on('select2:select', function(e){
+    $('#produ-sub-sections').append('<div data-taxonomy-id="' + e.params.data.id + '" class="jstree_produ_div"></div>');
+    $('[data-taxonomy-id=' + e.params.data.id + ']')
+    .on('select_node.jstree', function(e, data){
+      let parentId = $(e.target).data('taxonomy-id');
+
+      if ( typeof PRODU_DATA.subCategories === 'undefined' ) {
+        PRODU_DATA.subCategories = {};
+      }
+
+      if ( typeof PRODU_DATA.subCategories['cat_' + parentId] === 'undefined' ) {
+        PRODU_DATA.subCategories['cat_' + parentId];
+      }
+
+      PRODU_DATA.subCategories['cat_' + parentId] = data.selected;
+
+      $('input[name=produ-sub-categories]').val(JSON.stringify(PRODU_DATA.subCategories));
+
+      // TODO: Define what to do when all items are selected.
+      //console.log((data.node.children.length > 0));
+    })
+    .on('deselect_node.jstree', function(e, data){
+      let parentId = $(e.target).data('taxonomy-id');
+
+      PRODU_DATA.subCategories['cat_' + parentId] = data.selected;
+      
+      $('input[name=produ-sub-categories]').val(JSON.stringify(PRODU_DATA.subCategories));
+      console.log($('input[name=produ-sub-categories]').val());
+    })
+    .jstree(
+      {
+        'core' : {
+          'data' : {
+            "url" : PRODU_DATA.tax_endpoint + e.params.data.id,
+            "dataType" : "json",
+          }
         },
-        ajaxResults: function (json) {
-          return json;
-        }
-    }
+        'plugins': ['checkbox']
+      }
+    );
+  });
 
-    let select = field.$el.find('select');
-    acf.select2.init( select, props, field );
-
-    select.on('select2:select', function(e){
-      jQuery('#produ-sub-sections').append('<div data-taxonomy-id="' + e.params.data.id + '" class="jstree_produ_div"></div>');
-      jQuery('[data-taxonomy-id=' + e.params.data.id + ']').jstree(
-        {
-          'core' : {
-            'data' : {
-              "url" : PRODU_DATA.tax_endpoint + e.params.data.id,
-              "dataType" : "json",
-            }
-          },
-          'plugins': ['checkbox']
-        }
-      );
-    });
-
-    select.on('select2:unselect', function(e){
-      jQuery('[data-taxonomy-id=' + e.params.data.id + ']').remove();
-    });
+  //What to do when a main category is unselected.
+  PRODU_DATA.select.on('select2:unselect', function(e){
+    $('[data-taxonomy-id=' + e.params.data.id + ']').remove();
+  });
 });
 
-jQuery(function ($) { 
-  $('.jstree_demo_div').jstree(
-      {
-          'plugins': ['checkbox']
+//Initialize the main section selector.
+acf.addAction('new_field/type=produCustomTaxonomyField', function( field ){
+  let props = {
+      allowNull: false,
+      placeholder: 'Select',
+      multiple: true,
+      ajax: true,
+      ajaxAction: 'acf/fields/taxonomy/query',
+      ajaxData: function (data) {
+        data.field_key = field.get('key');
+        return data;
+      },
+      ajaxResults: function (json) {
+        return json;
       }
-  );
+  }
+
+  let select = field.$el.find('select');
+  acf.select2.init( select, props, field );
+
+  if ( typeof PRODU_DATA === 'undefined' ) {
+    const PRODU_DATA = {};
+  }
+  PRODU_DATA.select = select;
 });
--- templates/taxonomy-field.php
+++ templates/taxonomy-field.php
@@ -2,4 +2,6 @@
 <div <?php echo $attributes; ?>>
 	<?php acf_render_field( $field ); ?>
 </div>
-<div id="produ-sub-sections" style="display: flex; justify-content: space-around; flex-wrap: wrap;"></div>
+<div id="produ-sub-sections" style="display: flex; justify-content: space-around; flex-wrap: wrap;">
+	<input type="hidden" name="produ-sub-categories">
+</div>
